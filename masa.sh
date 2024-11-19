f6f44f07407213f7c166817c57cf70f889e6f4bf
Luckyxg <87571231+GzGod@users.noreply.github.com>
2024-11-20 00:41:54 +0800

    Update masa.sh

--- masa.sh
+++ masa.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-# 安装路径
 MASA_ORACLE_PATH="$HOME/masa-oracle"
 
-# 检查是否以root用户运行脚本
 if [ "$(id -u)" != "0" ]; then
     echo "此脚本需要以root用户权限运行。"
     echo "请尝试使用 'sudo -i' 命令切换到root用户，然后再次运行此脚本。"
@@ -12,7 +10,6 @@ fi
 
 apt install make sudo curl -y
 
-# 主菜单
 function main_menu() {
     clear
     echo "╔═╗╔═╦╗─╔╦═══╦═══╦═══╦═══╗" 
@@ -37,14 +34,13 @@ function main_menu() {
     esac
 }
 
-# 检查并安装 Go
 function install_go() {
     echo "更新软件包列表..."
     sudo apt update
 
     if command -v go > /dev/null 2>&1; then
         echo "Go 已安装，版本: $(go version)"
-        GO_VERSION=$(go version | awk '{print $3}' | cut -d. -f1-2)  # 获取主版本和次版本
+        GO_VERSION=$(go version | awk '{print $3}' | cut -d. -f1-2)
         if [ "$(printf '%s\n' "1.23.0" "$GO_VERSION" | sort -V | head -n1)" == "1.23.0" ]; then
             echo "Go 版本符合要求: $GO_VERSION"
         else
@@ -62,7 +58,6 @@ function install_go() {
     go version
 }
 
-# 检查并安装 Node.js 和 npm
 function install_nodejs_and_npm() {
     if command -v node > /dev/null 2>&1; then
         echo "Node.js 已安装，版本: $(node -v)"
@@ -80,7 +75,6 @@ function install_nodejs_and_npm() {
     fi
 }
 
-# 检查并安装 PM2
 function install_pm2() {
     if command -v pm2 > /dev/null 2>&1; then
         echo "PM2 已安装，版本: $(pm2 -v)"
@@ -90,7 +84,6 @@ function install_pm2() {
     fi
 }
 
-# 检查并安装 Git
 function install_git() {
     if command -v git > /dev/null 2>&1; then
         echo "Git 已安装，版本: $(git --version)"
@@ -100,20 +93,17 @@ function install_git() {
     fi
 }
 
-# 克隆 Masa Oracle 合约
 function clone_masa_oracle() {
     echo "克隆 Masa Oracle 合约..."
-    git clone -b v0.8.6  https://github.com/masa-finance/masa-oracle.git "$MASA_ORACLE_PATH"
+    git clone -b v0.8.6 https://github.com/masa-finance/masa-oracle.git "$MASA_ORACLE_PATH"
     cd "$MASA_ORACLE_PATH/contracts" || exit
     echo "安装 npm 依赖..."
     npm install
 }
 
-# 创建 .env 文件
 function create_env_file() {
     echo "创建 .env 文件..."
     
-    # 让用户输入推特账号和密码
     read -p "请输入推特账号（格式：username:password）: " TWITTER_ACCOUNTS
 
     cat <<EOL > "$MASA_ORACLE_PATH/.env"
@@ -131,42 +121,35 @@ API_ENABLED=true
 EOL
 }
 
-# 打包
 function build() {
     echo "编译程序..."
     cd "$MASA_ORACLE_PATH" || exit
     make build
 }
 
-# 运行节点并获取钱包地址
 function run_node() {
     echo "启动节点..."
     
-    # 在后台运行 make run，并将输出重定向到日志文件
     make run > "masa_output.log" 2>&1 & 
-    NODE_PID=$!  # 获取后台进程的 PID
+    NODE_PID=$!
     echo "节点已启动，PID: $NODE_PID"
     
-    # 循环检查日志文件，直到找到公钥
     while true; do
-        # 检查日志文件中是否包含公钥
         PUBLIC_KEY=$(grep "Public Key:" "masa_output.log" | awk '{print $NF}' | tail -n 1)
         
         if [ -n "$PUBLIC_KEY" ]; then
             echo "您的钱包地址是: $PUBLIC_KEY"
-            break  # 找到公钥后退出循环
+            break
         fi
         
         echo "正在等待公钥生成..."
-        sleep 5  # 每 5 秒检查一次
+        sleep 5
     done
 
-    # 停止节点
-    kill $NODE_PID  # 停止后台进程
+    kill $NODE_PID
     echo "节点已停止。"
 }
 
-# Faucet 步骤
 function faucet() {
     echo "在执行 Faucet 请求之前，请确保您已将 Sepolia ETH 测试币转入钱包地址。"
     read -p "请确认您已转账0.1ETH 并输入 'yes' 继续执行 Faucet 请求: " CONFIRM
@@ -179,34 +162,27 @@ function faucet() {
     fi
 }
 
-# 质押步骤
 function stake() {
     echo "进行 Stake..."
     cd "$MASA_ORACLE_PATH" || exit
     make stake
 }
 
-# 重新启动节点
 function restart_node() {
     echo "重新启动节点..."
     pm2 start make --name masa -- run
 }
 
-# 查看日志
 function view_logs() {
     echo "查看 PM2 日志..."
     pm2 logs masa
 }
 
-# 获取地址
 function input_multiaddress() {
-
-MULTIADDRESS=$(grep "Multiaddress:" "/root/masa-oracle/masa_output.log" | awk '{print $NF}' | tail -n 1)
-echo "Multiaddress 是: $MULTIADDRESS"
-
+    MULTIADDRESS=$(grep "Multiaddress:" "$MASA_ORACLE_PATH/masa_output.log" | awk '{print $NF}' | tail -n 1)
+    echo "Multiaddress 是: $MULTIADDRESS"
 }
 
-# 主安装函数
 function install_masa_oracle() {
     install_go
     install_nodejs_and_npm
@@ -222,5 +198,4 @@ function install_masa_oracle() {
     echo "Masa Oracle 安装和配置完成！"
 }
 
-# 显示主菜单
 main_menu
